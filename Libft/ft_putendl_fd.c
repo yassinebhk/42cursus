748b64e640537d22c4748c5388d86da218ef0a2b
Yassine Bouhaik <72581389+yassinebhk@users.noreply.github.com>
2023-09-15 00:19:15 +0200

    Update ft_putendl_fd.c

--- Libft/ft_putendl_fd.c
+++ Libft/ft_putendl_fd.c
@@ -10,4 +10,11 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
+void ft_putendl_fd(char *s, int fd)
+{
+    write(fd, s, ft_strlen (s));
+    write(fd, "\n", 1);
+}
 
