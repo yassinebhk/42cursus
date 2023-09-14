0cb429513e23bbf734f79118af43da2ded6769c6
Yassine Bouhaik <72581389+yassinebhk@users.noreply.github.com>
2023-09-15 00:22:08 +0200

    Update ft_putnbr_fd.c

--- Libft/ft_putnbr_fd.c
+++ Libft/ft_putnbr_fd.c
@@ -10,4 +10,13 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
+void ft_putnbr_fd(int n, int fd)
+{
+    char *num;
+
+    num = ft_itoa (n);
+    write(fd, num, ft_strlen (num));
+}
 
