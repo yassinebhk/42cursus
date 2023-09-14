41079a0e8b6a6e882562641b7c86db680dd1cf8d
Yassine Bouhaik <72581389+yassinebhk@users.noreply.github.com>
2023-09-15 00:16:31 +0200

    Update ft_putstr_fd.c

--- Libft/ft_putstr_fd.c
+++ Libft/ft_putstr_fd.c
@@ -10,4 +10,10 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
+void ft_putstr_fd(char *s, int fd)
+{
+    write(fd, s, ft_strlen (s));
+}
 
