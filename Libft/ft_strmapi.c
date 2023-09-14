ebf4d5f9a0f247bf1047c719b21397e1a76d0107
Yassine Bouhaik <72581389+yassinebhk@users.noreply.github.com>
2023-09-14 23:46:41 +0200

    Update ft_strmapi.c

--- Libft/ft_strmapi.c
+++ Libft/ft_strmapi.c
@@ -10,4 +10,23 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 
+char    *ft_strmapi (char const *s, char (*f)(unsigned int, char))
+{
+    char *str;
+    unsigned int i;
+    
+    i = 0;
+    str = (char *) malloc ((ft_strlen ((char *)s) + 1) * sizeof (char));
+    if (*str)
+        return (NULL);
+    while (i < ft_strlen ((char *)s))
+    {
+        *(str + i) = (*f)(i, *(s + i));
+        i++;
+    }
+
+    *(str + i) = '\0';
+    return (str);
+}
