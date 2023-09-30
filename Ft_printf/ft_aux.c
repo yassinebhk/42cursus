beccce1695a1acca9e770b441fcd4e51b715c9c6
Yassine Bouhaik <72581389+yassinebhk@users.noreply.github.com>
2023-09-30 23:24:22 +0200

    Add files via upload

--- Ft_printf/ft_aux.c
+++ Ft_printf/ft_aux.c
@@ -12,44 +12,6 @@
 
 #include "ft_printf.h"
 
-int	ft_putstr_fd(char *s, int *res)
-{
-	int	check;
-
-	if (!s)
-		check = write(1, "(null)", 6);
-	else
-		check = write(1, s, ft_strlen(s));
-	if (check == -1)
-		return (0);
-	else
-	{
-		*res += check;
-		return (1);
-	}
-}
-
-size_t	ft_strlen(const char *s)
-{
-	size_t	cont;
-
-	cont = 0;
-	while (s[cont] != '\0')
-		cont++;
-	return (cont);
-}
-
-int	ft_putchar_fd(char c, int *res)
-{
-	if (write(1, &c, sizeof(char)) == -1)
-		return (0);
-	else
-	{
-		*res += 1;
-		return (1);
-	}
-}
-
 int	putnbr(long long n, int *res, char *base)
 {
 	char	a;
