d79c31397657feef7c71587b819b9e162a714289
Yassine Bouhaik Bouhoussaine <ybouhaik@c3r4s1.42malaga.com>
2024-08-05 21:57:16 +0200

    Philosophers

---
+++ philosophers/philo/src/philo.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ybouhaik <ybouhaik@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/05 19:40:01 by ybouhaik          #+#    #+#             */
+/*   Updated: 2024/08/05 21:50:56 by ybouhaik         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/philo.h"
+
+int main (int argc, char **argv)
+{
+    (void)argc;
+    (void)argv;
+    // if (!check_args(argc, argv))
+    // {
+    //     // no se puede utilziar exit, utilziar flags
+    // }
+    return (0);
+}
