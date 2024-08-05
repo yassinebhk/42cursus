8648ed5dbe6e981af4e57aa28e3362554a9df84a
Yassine10bhk <yassinebouhaik10@gmail.com>
2024-08-05 23:55:53 +0200

    Included the needed libraries

--- philosophers/philo/include/philo.h
+++ philosophers/philo/include/philo.h
@@ -5,8 +5,20 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: ybouhaik <ybouhaik@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/08/05 19:39:49 by ybouhaik          #+#    #+#             */
-/*   Updated: 2024/08/05 19:39:50 by ybouhaik         ###   ########.fr       */
+/*   Created: 2024/08/05 23:50:15 by ybouhaik          #+#    #+#             */
+/*   Updated: 2024/08/05 23:52:51 by ybouhaik         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+# ifndef PHILO_H
+# define PHILO_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <unistd.h>
+# include <unistd.h>
+# include <sys/time.h>
+# include <pthread.h>
+
+# endif
