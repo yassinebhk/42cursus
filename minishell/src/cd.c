2bb6d03bbe2057b8b95fe2a66fa8dd90da9df625
Máximo García Aroca <r56bj1217@gmail.com>
2024-08-21 12:38:43 +0200

    cd update complited: new function change_directory saves lines

--- minishell/src/cd.c
+++ minishell/src/cd.c
@@ -6,7 +6,7 @@
 /*   By: ybouhaik <ybouhaik@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/17 12:23:15 by ybouhaik          #+#    #+#             */
-/*   Updated: 2024/08/21 12:16:42 by maxgarci         ###   ########.fr       */
+/*   Updated: 2024/08/21 12:33:59 by maxgarci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,13 +24,14 @@ char	*get_env(t_env *env, char *key)
 	return (NULL);
 }
 
-static int	move_to_path_env(char *new_dir, t_env *env, t_env *exp,
-		char *old_dir)
+static int	move_to_path_env(char *new_dir, t_env *env, t_env *exp)
 {
 	char	*new_rel_dir;
 	char	*tmp;
 	char	*cwd;
+	char	*old_dir;
 
+	old_dir = getcwd(NULL, 0);
 	tmp = ft_strjoin("/", new_dir);
 	cwd = getcwd(NULL, 0);
 	new_rel_dir = ft_strjoin(cwd, tmp);
@@ -51,39 +52,46 @@ static int	move_to_path_env(char *new_dir, t_env *env, t_env *exp,
 	return (free(cwd), free(tmp), free(new_rel_dir), 0);
 }
 
-int	cd_env(char **str, int num_words, t_env *env, t_env *exp)
+static int	change_directory(t_env *env, t_env *exp, char *var, int flag)
 {
 	char	*old_dir;
 	char	*new_dir;
 
 	old_dir = getcwd(NULL, 0);
-	if (num_words == 1 || !ft_strcmp(str[1], "~"))
+	if (flag)
 	{
-		new_dir = get_env(env, "HOME\0");
+		new_dir = get_env(env, var);
 		if (!new_dir)
 			return (1);
-		chdir(new_dir);
-		update(env, exp, old_dir, new_dir);
+	}
+	else
+		new_dir = get_parent(old_dir);
+	chdir(new_dir);
+	update(env, exp, old_dir, new_dir);
+	if (!flag)
+		free(new_dir);
+	return (0);
+}
+
+int	cd_env(char **str, int num_words, t_env *env, t_env *exp)
+{
+	if (num_words == 1 || !ft_strcmp(str[1], "~"))
+	{
+		if (change_directory(env, exp, "HOME\0", 1))
+			return (1);
 	}
 	else if (!ft_strcmp(str[1], "..\0"))
 	{
-		new_dir = get_parent(old_dir);
-		chdir(new_dir);
-		update(env, exp, old_dir, new_dir);
-		free(new_dir);
+		if (change_directory(env, exp, NULL, 0))
+			return (1);
 	}
-	else if (!ft_strcmp(str[1], "."))
-		free(old_dir);
 	else if (!ft_strcmp(str[1], "-"))
 	{
-		new_dir = get_env(env, "OLDPWD\0");
-		if (!new_dir)
+		if (change_directory(env, exp, "OLDPWD\0", 1))
 			return (1);
-		chdir(new_dir);
-		update(env, exp, old_dir, new_dir);
 	}
-	else
-		return (move_to_path_env(str[1], env, exp, old_dir));
+	else if (ft_strcmp(str[1], "."))
+		return (move_to_path_env(str[1], env, exp));
 	return (0);
 }
 
