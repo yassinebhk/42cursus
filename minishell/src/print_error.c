904137f7d685c90954ae483f209d3394b4f431ce
Yassine10bhk <yassinebouhaik10@gmail.com>
2024-08-22 16:58:21 +0200

    Modified split_by_spaces() function. If occurs any error, now we call to the print_error function by passing it the command and the errno

--- minishell/src/print_error.c
+++ minishell/src/print_error.c
@@ -1,24 +1,12 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   print_error.c                                      :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: ybouhaik <ybouhaik@student.42malaga.com    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/08/19 18:10:01 by maxgarci          #+#    #+#             */
-/*   Updated: 2024/08/21 23:01:49 by maxgarci         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "minishell.h"
 
 void	print_error(char *command, int errno)
 {
-	if (errno == 12)
+	if (errno == ENO_MEM)
 		ft_putstr_fd(" allocation failed, no memory available: ", 2);
-	if (errno == 120)
+	if (errno == BAD_ASSIGNMENT)
 		ft_putstr_fd(" bad assignment: ", 2);
-	if (errno == 127)
+	if (errno == COMMAND_NOT_FOUND)
 		ft_putstr_fd("command not found: ", 2);
 	ft_putstr_fd(command, 2);
 	ft_putchar_fd('\n', 2);
