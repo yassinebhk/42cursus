3137513670481150f70551ea79a5b5789031f0a4
Yassine10bhk <yassinebouhaik10@gmail.com>
2024-08-29 03:08:45 +0200

    Fixed even_quotes() function

--- minishell/src/parsing.c
+++ minishell/src/parsing.c
@@ -1,29 +1,57 @@
 #include "minishell.h"
 
-int	even_quotes(char *line)
+void	count_quotes(const char *line, int *single_quote_count,
+		int *double_quote_count)
 {
 	int	pos;
-	int	single_quote;
-	int	double_quote;
+	int	single_quote_open;
+	int	double_quote_open;
 
 	pos = -1;
-	single_quote = 0;
-	double_quote = 0;
+	single_quote_open = 0;
+	double_quote_open = 0;
+	*single_quote_count = 0;
+	*double_quote_count = 0;
 	while (line[++pos])
 	{
-		if (line[pos] == SINGLE_QUOTE)
-			single_quote++;
-		else if (line[pos] == DOUBLE_QUOTE)
-			double_quote++;
+		if (line[pos] == SINGLE_QUOTE && !double_quote_open)
+		{
+			single_quote_open = !single_quote_open;
+			(*single_quote_count)++;
+		}
+		else if (line[pos] == DOUBLE_QUOTE && !single_quote_open)
+		{
+			double_quote_open = !double_quote_open;
+			(*double_quote_count)++;
+		}
 	}
-	if (single_quote % 2 != 0)
+	*single_quote_count = single_quote_open;
+	*double_quote_count = double_quote_open;
+}
+
+int	check_quotes_balance(int single_quote_count, int double_quote_count)
+{
+	if (single_quote_count % 2 != 0)
 	{
 		ft_putstr_fd(SINGLE_QUOTE_ERROR, 2);
 		return (0);
 	}
-	else if (double_quote % 2 != 0)
+	if (double_quote_count % 2 != 0)
 	{
 		ft_putstr_fd(DOUBLE_QUOTE_ERROR, 2);
+		return (0);
+	}
+	return (1);
+}
+
+int	even_quotes(char *line)
+{
+	int	single_quote_count;
+	int	double_quote_count;
+
+	count_quotes(line, &single_quote_count, &double_quote_count);
+	if (!check_quotes_balance(single_quote_count, double_quote_count))
+	{
 		return (0);
 	}
 	return (1);
